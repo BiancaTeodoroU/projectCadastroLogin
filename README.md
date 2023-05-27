65064a905b2d1d43a85e62115feac209600137f0
biancateodoro <biancaurtado645@gmail.com>
2023-05-27 12:03:05 -0300

    chore: ch: changed in project

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
-# Projeto de Tela de cadastro e login
+# Sistema de listagem de usuários
 
-## Tecnologias que serão utilizadas para o desenvolvimento
+## Objetivo: Com esse novo projeto o usuário deve entrar direto na home, e nela terá um botão para adicionar novas informações de usuários detalhe que os campos terão a validação de que os campos não podem estar em branco o email tem que ter o @, quando adicionada a nova informação o usuário será redirecionado novamente para a home, e lá estara listado o usuário novo que foi cadastro, e será possivel editar ou excluir.
 
-### html, css e javascript, e banco usarei o Mysql Workbench
+### As tecnologias utilizadas para essa aplicação serão: NodeJS, ExpressJS , MySQL, ejs, bootstrap, bodyParser, path.
 
-#### A ideia do projeto é que seja feita uma tela de cadastro e uma de login, e que após o cadastro, para poder ser editada alguma informação do cadastro dele, ele terá que fazer login, mas caso o usuário nem lembre a senha, ele poderá trocar, após o login ele poderá editar todos os dados como o nome de usuário e senha, somente o email não poderá ser trocado, e caso o usuário não queira mais ter o cadastro na página ele terá a opção de excluir a conta.
+### Objetivo da aplicação: O objetivo é ter uma aplicação simples mas fácil de utilizar para o usuário final.
 
-#### Etapas
+### para iniciar o servidor usar o comando
 
-#### Fazer o cadastro
-
-#### Ter uma verificação no formulário de cadastro, que o usuário deverá colocar uma quantidade minima de caracteres, não poderá deixar nenhum campo em branco, e uma validação no email para cadastrar um email correto, por exemplo ele não aceitar que o email seja "fulano.com" só aceitar somente quando tiver "fulano@gmail.com".
-
-#### Após o cadastro irá aparecer uma mensagem de que o cadastro foi feito com sucesso
-
-#### Após o cadastro o usuário irá para tela de Login
-
-#### Caso esqueça a senha terá um campo que ele poderá clicar para renomear a senha.
-
-#### Após o usuário fazer login, ele será encaminhado para uma tela na qual ele poderá editar os dados nome ou senha só não irá conseguir trocar o email, e caso queira poderá excluir a sua conta.
-
-#### O objetivo da aplicação é aprender mais sobre como é feito o desenvolvimento de uma tela de cadastro e login, como persistir os dados, como fazer o front-end e o back-end se comunicarem.
+    nodemon app
---
+++ app.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    port: '3306',
+    password:'bianca',
+    database:'node_crud'
+});
+
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Conectado!');
+}); 
+
+//set views file
+app.set('views',path.join(__dirname,'views'));
+
+//set view engine
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',(req, res) => {
+    let sql = "SELECT * FROM users";
+    let query = connection.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('user_index', {
+            title : 'Hello world! Welcome to my system 😊',
+            users : rows
+        });
+    });
+});
+
+app.get('/add',(req, res) => {
+    res.render('user_add', {
+        title : 'Adicionar usúarios 😜'
+    });
+});
+
+app.post('/save',(req, res) => { 
+    let data = {name: req.body.name, email: req.body.email, phone_no: req.body.phone_no};
+    let sql = "INSERT INTO users SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/edit/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `Select * from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('user_edit', {
+            title : 'Editar informações do usúario 📝',
+            user : result[0]
+        });
+    });
+});
+
+app.post('/update',(req, res) => {
+    const userId = req.body.id;
+    let sql = "update users SET name='"+req.body.name+"',  email='"+req.body.email+"',  phone_no='"+req.body.phone_no+"' where id ="+userId;
+    let query = connection.query(sql,(err, results) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+// server
+
+app.listen(3000, () => {
+    console.log('server irá ligar na porta 3000');
+});
